106ad2e2a951d3e43a6bb4f3038b17d574b848cb
Anushree Goswami <agoswami@Anushrees-MacBook-Pro.local>
2022-03-31 15:12:27 -0400

    added bowlers dataset and some related functions

---
+++ R/overs.R
@@ -0,0 +1,25 @@
+#' Calculates the number of overs for the given bowler
+#'
+#' @param player The bowler whose overs has to be calculated, as a character vector
+#'
+#' @return `overs` returns the number of overs
+#'
+#' @examples
+#'
+#' library(ipl)
+#'
+#' # Calculates the number of overs for Rahul Sharma
+#' overs("Rahul Sharma")
+#' @importFrom magrittr "%>%"
+#' @import dplyr
+#'
+#' @export
+#'
+overs <- function(player) {
+  if (player %in% bowlers$PLAYER) {
+    return(bowlers$Ov[bowlers$PLAYER == player])
+  }
+  else {
+    stop("Invalid player name, please input another name")
+  }
+}
