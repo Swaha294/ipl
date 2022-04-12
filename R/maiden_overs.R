e8328333af4e5660079b54c8aed2b77baadccc35
Anushree Goswami <agoswami@Anushrees-MacBook-Pro.local>
2022-04-12 11:57:09 -0400

    maiden_overs() function

---
+++ R/maiden_overs.R
@@ -0,0 +1,23 @@
+#' Returns the maiden overs for the given bowler
+#'
+#' @param player_name The bowler whose maiden overs has to be calculated, as a
+#' character vector
+#'
+#' @return `maidenOvers` returns the maiden overs
+#'
+#' @examples
+#'
+#' library(ipl)
+#'
+#' # Calculates the maiden overs for Rahul Sharma
+#' maiden_overs("Rahul Sharma")
+#' @importFrom magrittr "%>%"
+#' @import dplyr
+#'
+#' @export
+#'
+maiden_overs <- function(player_name) {
+  maidenOvers = 0
+  return(0)
+}
+
