dcdc4680259ae4c02cd1ac830c3873e99989312a
Swaha294 <sbhattacharya@smith.edu>
2022-04-19 11:44:37 -0400

    variables in overs_balls fixed, function added to README.Rmd

---
+++ R/overs_balls.R
@@ -0,0 +1,36 @@
+#' Calculates the completed overs and remaining (legal) balls bowled by
+#' the given bowler across all IPL matches from 2008-2020.
+#'
+#' @param player_name The bowler whose completed overs and remaining balls has to
+#' be calculated, as a character vector
+#'
+#' @return `overs_balls` returns the number of completed overs and remaining balls
+#' bowled by the given player across all IPL matches from 2008-2020
+#'
+#' @examples
+#'
+#' library(ipl)
+#'
+#' # Calculates the completed runs and remaining (legal) balls bowled by Rahul Sharma
+#' overs_balls("Rahul Sharma")
+#' @importFrom magrittr "%>%"
+#' @import dplyr
+#'
+#' @export
+#'
+overs_balls <- function(player_name) {
+  if (!(is.character(player_name))) {
+    stop(paste("Invalid input: ", player_name, " should be a character vector"), call. = FALSE)
+  } else if (!(player_name %in% bowlers$player)) {
+    stop("Invalid player name, please input another name", call. = FALSE)
+  } else {
+    overs <- bowlers$ov[bowlers$player == player_name]
+    overs <- as.character(overs)
+    split_overs <- unlist(strsplit(overs, "\\."))
+    player <- player_name
+    completed_overs <- split_overs[1]
+    balls <- split_overs[2]
+    output_df <- data.frame(player, completed_overs, balls)
+    return(output_df)
+  }
+}
---
+++ R/partnership_runs.R
@@ -0,0 +1,4 @@
+#' Calculate the runs made by all batting partnerships in a given IPL match for
+#' a given team
+#'
+#'
