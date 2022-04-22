f9c1f3a2737a8ad7f57fec69642e8c5decf8057b
Swaha294 <sbhattacharya@smith.edu>
2022-04-22 14:50:49 -0400

    typo

--- data-raw/sticker.R
+++ data-raw/sticker.R
@@ -1,11 +1,35 @@
 library(hexSticker)
-library(jpeg)
 library(magick)
 library(dplyr)
 library(showtext)
 
 
+font_add_google("Akshar", family = "Sans Serif", db_cache = FALSE)
 
+img <- image_read("data-raw/IPL-PNG-Logo.jpg")
+
+res <- img %>%
+  image_convert("png") %>%
+  image_resize("1080 x 200")
+
+
+ipl_sticker <- sticker(
+  subplot = res,
+  s_x = 1,
+  s_height = 1.2,
+  s_width = 1.2,
+  package = "ipl",
+  p_color = "#191970",
+  #p_color = "#252525",
+  h_fill = "#FFFFFF",
+  h_color = "#191970",
+  h_size = 1.5,
+  p_y = 1.6,
+  p_family = "Sans Serif"
+)
+
+
+save_sticker(here::here("data-raw", "hex_ipl.png"), ipl_sticker)
 
 
 
