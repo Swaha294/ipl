e31613a57fcdbe8f9a3af03033c3ab698b5f48c2
Anushree Goswami <agoswami@Anushrees-MacBook-Pro.local>
2022-04-26 13:06:40 -0400

    commit

--- R/maiden_overs.R
+++ R/maiden_overs.R
@@ -16,8 +16,27 @@
 #'
 #' @export
 #'
-maiden_overs <- function(player_name) {
-  maidenOvers = 0
-  return(0)
+bowling_analysis <- function(player_name) {
+  overs = overs(player_name)
+
+  #maiden overs naming
+  firstname_letter = substr(word(player_name, 1), 1, 1)
+  surname = word(player_name, 2)
+  player_name_MO = paste(firstname_letter, surname)
+  maiden_overs = maiden_overs(player_name_MO)
+
+  runs = runs(player_name)
+  wickets_taken = wickets_taken(player_name)
+  bowlingAnalysis = paste(overs, "-", maiden_overs, "-",runs, "-", wickets_taken)
+
+  Player <- player_name
+  Overs <- overs
+  Maiden_Overs <- maiden_overs
+  Runs <- runs
+  Wickets_Taken <- wickets_taken
+  Bowling_Analysis <- bowlingAnalysis
+
+  output_df <- data.frame(Player, Overs, Maiden_Overs, Runs, Wickets_Taken, Bowling_Analysis)
+  return(output_df)
 }
 
